0b3f98bd1216053967692e7d69e8fed49c6fa2f1
AndresBaez96 <afbaezm@unal.edu.co>
2021-07-21 20:36:49 -0500

    Update reto 1

--- src/Retos/Proyecto.java
+++ src/Retos/Proyecto.java
@@ -11,15 +11,18 @@ public class Proyecto {
     }
 
     public String compararPresupuestos(int pTiempo, double pMonto, double pInteres){
-        try {
-            double interesSimple = pMonto * pInteres * pTiempo;
-            double interesCompuesto = pMonto * (Math.pow(1 + pInteres, pTiempo)- 1);
-            double compararPresupuestos = interesCompuesto - interesSimple;
-            return "La diferencia en el total de intereses generados para el proyecto, si escogemos entre evaluarlo a una tasa de interés Compuesto y evaluarlo a una tasa de interés Simple, asciende a la cifra de: $"+ compararPresupuestos;
-        } catch (Exception e) {
-            String a = "a";
-            return a;
+        double interesSimple = pMonto * pInteres * pTiempo;
+        double interesCompuesto = pMonto * (Math.pow(1 + pInteres, pTiempo)- 1);
+        double compararPresupuestos = interesCompuesto - interesSimple;
+
+        if (compararPresupuestos > 0) {
+            return "La diferencia en el total de intereses generados para el proyecto, si escogemos entre evaluarlo a una tasa de interés Compuesto y evaluarlo a una tasa de interés Simple, asciende a la cifra de: $"+ compararPresupuestos; 
+        } else {
+            return "Faltan datos para calcular la diferencia en el total de interes generados para el proyecto";
         }
+        
+
+        
         
     }
 }
