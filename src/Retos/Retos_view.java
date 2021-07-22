40e052b1dd60208a2746089148a0e290c51fe445
AndresBaez96 <afbaezm@unal.edu.co>
2021-07-21 20:40:50 -0500

    Código Reto 1

--- src/Retos/Retos_view.java
+++ src/Retos/Retos_view.java
@@ -3,7 +3,7 @@ package Retos;
 public class Retos_view {
     public static void main(String[] args) {
         Proyecto resultadoInversion = new Proyecto();
-        System.out.println(resultadoInversion.compararPresupuestos(1, 500000, 0.5));
+        System.out.println(resultadoInversion.compararPresupuestos(1, 200000, 0.5));
 
     }
 }
