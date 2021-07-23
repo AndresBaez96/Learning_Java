32278e4a39bb3efd040b83d12b5dd4ca3b4a4e78
AndresBaez96 <afbaezm@unal.edu.co>
2021-07-22 21:21:07 -0500

    Reto 1 finalizado

--- src/Retos/Proyecto.java
+++ src/Retos/Proyecto.java
@@ -10,19 +10,15 @@ public class Proyecto {
         super();
     }
 
-    public String compararPresupuestos(int pTiempo, double pMonto, double pInteres){
+    public String compararProyectos(int pTiempo, double pMonto, double pInteres){
         double interesSimple = pMonto * pInteres * pTiempo;
         double interesCompuesto = pMonto * (Math.pow(1 + pInteres, pTiempo)- 1);
         double compararPresupuestos = interesCompuesto - interesSimple;
 
-        if (compararPresupuestos > 0) {
-            return "La diferencia en el total de intereses generados para el proyecto, si escogemos entre evaluarlo a una tasa de interés Compuesto y evaluarlo a una tasa de interés Simple, asciende a la cifra de: $"+ compararPresupuestos; 
+        if (compararPresupuestos >= 0) {
+            return "Resultado "+ compararPresupuestos; 
         } else {
             return "Faltan datos para calcular la diferencia en el total de interes generados para el proyecto";
-        }
-        
-
-        
-        
+        }  
     }
 }
