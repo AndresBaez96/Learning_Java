3dc816ef2a778ff8bb98a9dc45ad126a09265141
AndresBaez96 <afbaezm@unal.edu.co>
2021-07-31 09:45:04 -0500

    Update Semana 5

--- src/view/Files.java
+++ src/view/Files.java
@@ -1,6 +1,8 @@
 package view;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
@@ -10,13 +12,24 @@ public class Files {
         OutputStream output = new FileOutputStream("./data/data.properties"); //Llama a una carpeta donde se va a guardar el archivo
         Properties prop = new Properties(); 
         prop.setProperty("nombre", "Andres");
-        prop.setProperty("edad ", "25");
+        prop.setProperty("edad", "25");
         prop.store(output, null); // Guarda las propiedades seteadas anteriormente
-        
         System.out.println(prop);
+        output.close();
 
     } catch (Exception e) {
         
+    }
+    try {
+        InputStream input = new FileInputStream("./data/data.properties"); // Llama a la carpeta donde se guardan los datos
+        Properties prop = new Properties();
+        prop.load(input); 
+        System.out.println(prop.getProperty("nombre"));
+        System.out.println(prop.getProperty("edad"));
+        input.close();
+
+    } catch (Exception e) {
+        // Todo: handle exception
     }
 }
     
---
+++ src/view/Interfaz.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Interfaz extends JFrame {
+    public static void main(String[] args) {
+        Interfaz i = new Interfaz();
+        i.setVisible(true); // Interfaz visible
+
+    }
+    
+}
